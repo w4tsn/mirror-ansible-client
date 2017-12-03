5225a1ef4fdbb4e72a87075bec9a9157b46ada4a
Alexander Wellbrock <lionax@mailbox.org>
2017-12-03 01:43:19 +0100

    Refactor vscode nautilus-extension and enable for files

--- roles/vscode/files/open-code.py
+++ roles/vscode/files/open-code.py
@@ -14,19 +14,19 @@ require_version('Nautilus', '3.0')
 from gi.repository import Gio, GObject, Gtk, Nautilus
 
 
-TERMINAL = "code"
-TILIX_KEYBINDINGS = "com.microsoft.Code.Keybindings"
+COMMAND = "code"
+KEYBINDINGS = "com.microsoft.Code.Keybindings"
 GSETTINGS_OPEN_TERMINAL = "nautilus-open"
-REMOTE_URI_SCHEME = ['ftp', 'sftp']
 textdomain("code")
 _ = gettext
 
-
-def open_terminal_in_file(filename):
-  if filename:
-    call('{0} -w "{1}" &'.format(TERMINAL, filename), shell=True)
-  else:
-    call("{0} &".format(TERMINAL), shell=True)
+def run(file_):
+  if file_:
+    filename = Gio.File.new_for_uri(file_.get_uri()).get_path()
+    if filename:
+      call('{0} "{1}" &'.format(COMMAND, filename), shell=True)
+    else:
+      call("{0} &".format(COMMAND), shell=True)
 
 
 class OpenCodeShortcutProvider(GObject.GObject,
@@ -34,8 +34,8 @@ class OpenCodeShortcutProvider(GObject.GObject,
 
   def __init__(self):
     source = Gio.SettingsSchemaSource.get_default()
-    if source.lookup(TILIX_KEYBINDINGS, True):
-      self._gsettings = Gio.Settings.new(TILIX_KEYBINDINGS)
+    if source.lookup(KEYBINDINGS, True):
+      self._gsettings = Gio.Settings.new(KEYBINDINGS)
       self._gsettings.connect("changed", self._bind_shortcut)
       self._create_accel_group()
     self._window = None
@@ -55,7 +55,7 @@ class OpenCodeShortcutProvider(GObject.GObject,
 
   def _open_terminal(self, *args):
     filename = unquote(self._uri[7:])
-    open_terminal_in_file(filename)
+    run(filename)
 
   def get_widget(self, uri, window):
     self._uri = uri
@@ -70,22 +70,7 @@ class OpenCodeShortcutProvider(GObject.GObject,
 class OpenCodeExtension(GObject.GObject, Nautilus.MenuProvider):
 
   def _open_terminal(self, file_):
-    if file_.get_uri_scheme() in REMOTE_URI_SCHEME:
-      result = urlparse(file_.get_uri())
-      if result.username:
-        value = 'ssh -t {0}@{1}'.format(result.username,
-                                        result.hostname)
-      else:
-        value = 'ssh -t {0}'.format(result.hostname)
-      if result.port:
-        value = "{0} -p {1}".format(value, result.port)
-      if file_.is_directory():
-        value = '{0} cd "{1}" ; $SHELL'.format(value, result.path)
-
-      call('{0} -e "{1}" &'.format(TERMINAL, value), shell=True)
-    else:
-      filename = Gio.File.new_for_uri(file_.get_uri()).get_path()
-      open_terminal_in_file(filename)
+    run(file_)
 
   def _menu_activate_cb(self, menu, file_):
     self._open_terminal(file_)
@@ -101,21 +86,11 @@ class OpenCodeExtension(GObject.GObject, Nautilus.MenuProvider):
     print("Handling file: ", file_.get_uri())
     print("file scheme: ", file_.get_uri_scheme())
 
-    if file_.is_directory():
-
-      if file_.get_uri_scheme() in REMOTE_URI_SCHEME:
-        uri = file_.get_uri().decode('utf-8')
-        item = Nautilus.MenuItem(name='NautilusPython::open_remote_item',
-                                 label=_(u'Open Remote Code'),
-                                 tip=_(u'Open Remote Code '
-                                       'In').format(uri))
-        item.connect('activate', self._menu_activate_cb, file_)
-        items.append(item)
-
+    if file_:
       filename = file_.get_name().decode('utf-8')
       item = Nautilus.MenuItem(name='NautilusPython::open_file_item',
                                label=_(u'Open In Code'),
-                               tip=_(u'Open Code In {}').format(filename))
+                               tip=_(u'Open Code For {}').format(filename))
       item.connect('activate', self._menu_activate_cb, file_)
       items.append(item)
 
@@ -123,12 +98,6 @@ class OpenCodeExtension(GObject.GObject, Nautilus.MenuProvider):
 
   def get_background_items(self, window, file_):
     items = []
-    if file_.get_uri_scheme() in REMOTE_URI_SCHEME:
-      item = Nautilus.MenuItem(name='NautilusPython::open_bg_remote_item',
-                               label=_(u'Open Remote Code Here'),
-                               tip=_(u'Open Remote Code In This Directory'))
-      item.connect('activate', self._menu_activate_cb, file_)
-      items.append(item)
 
     item = Nautilus.MenuItem(name='NautilusPython::open_bg_file_item',
                              label=_(u'Open Code Here'),
